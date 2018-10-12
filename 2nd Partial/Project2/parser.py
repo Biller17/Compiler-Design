f124bb894942b30abd1ac2bd88bcfe3c042c3d61
Biller17 <abiller95@gmail.com>
2018-10-11 23:33:24 -0500

    more parsing functions

--- 2nd Partial/Project2/parser.py
+++ 2nd Partial/Project2/parser.py
@@ -5,7 +5,7 @@ from lexer import *
 currentToken = None
 
 class Node:
-    def __init__(self, type, childNodes, value):
+    def __init__(self, type, childNodes = None, value =None):
         self.type = type
         self.value = value
         if childNodes :
@@ -17,7 +17,9 @@ class Node:
 
 
 def nextToken():
-    currentToken = getToken()
+
+    global currentToken
+    currentToken = getToken()[0]
     return currentToken
 
 def parser(imprime = True):
@@ -27,7 +29,6 @@ def parser(imprime = True):
 
 
 def parseProgram():
-    getToken()
     return Node("program", parseDeclarationList())
 
 
@@ -35,24 +36,73 @@ def parseDeclarationList():
     if(currentToken == TokenType.ENDFILE):
         return []
 
+
     return([parseDeclaration()].append(parseDeclarationList()))
 
 
 def parseDeclaration():
-    if(parseTypeSpecifier()):
-        
+    print("declaration")
+    #assign int or void
+    childNodes = [parseTypeSpecifier()]
+    if(nextToken() == TokenType.ID):
+        childNodes.append(Node("ID"))
+        if(nextToken() == TokenType.OPEN_PARENTHESIS):
+            childNodes.append(parseFunDeclaration())
+            return (Node("fun-declaration", childNodes))
+        else:
+            childNodes.append(parseVarDeclaration())
+            return(Node("var-declaration", childNodes))
+
+    return None
+
+
+def parseFunDeclaration():
+    childNodes = [Node("(")]
+    params = parseParams()
+    if(params):
+        childNodes.append(params)
+        if(nextToken == TokenType.CLOSE_PARENTHESIS):
+            childNodes.append(Node(")"))
+            compoundStmt = parseCompountStmt()
+            if(compoundStmt):
+                childNodes.append(compoundStmt)
+                return childNodes
+    return None
+
+def parseVarDeclaration():
+    childNodes = []
+    if(nextToken() == TokenType.SEMICOLON):
+        childNodes.append(Node(";"))
+        return childNodes
+    elif(nextToken() == TokenType.OPEN_BRACKETS):
+        childNodes.append(Node("["))
+        if(nextToken() == TokenType.NUM):
+            childNodes.append(Node("NUM"))
+            if(nextToken() == TokenType.CLOSE_BRACKETS):
+                childNodes.append(Node("]"))
+                if(nextToken() == TokenType.SEMICOLON):
+                    childNodes.append(Node(";"))
+                    return childNodes
+    return None
+
+
+
+
+
+
+
 # def parseFactor():
 #     childNodes = []
 #     if(getToken() == TokenType.OPEN_PARENTHESIS):
 #
 
 def parseTypeSpecifier():
-    if(nextToken() == TokenType.INT):
+    token = nextToken()
+    if(token == TokenType.INT):
         return(Node("int"))
-    elif(nextToken() == TokenType.VOID):
+    elif(token == TokenType.VOID):
         return(Node("void"))
-    else:
-        return None
+    return None
 
 def parseCall():
     childNodes = []
