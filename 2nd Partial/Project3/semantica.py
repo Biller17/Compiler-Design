73d8646af7413f3eec3a49ddbd8fa7035b655ee9
Biller <abiller95@gmail.com>
2018-11-08 00:21:36 -0600

    recursive function

--- 2nd Partial/Project3/semantica.py
+++ 2nd Partial/Project3/semantica.py
@@ -1,13 +1,39 @@
 from parser import *
 
 
+#programa analizador de semantica Adrian Biller A01018940
 
+
+class ScopeNode:
+    def __init__(self, level):
+        self.level = level
+        self.symbolTable = []
+        self.childScope = []
+
+
+    def generateSymbolTable():
+        
+
+    def printTree(self, level = 0):
+        print((level*3)*'__', self.type)
+        # print(self.childNodes)
+        if(self.childNodes != []):
+            if(type(self.childNodes) == Node):
+                self.childNodes.printTree()
+            else:
+                for i in range(len(self.childNodes)):
+                    if(type(self.childNodes[i]) == list):
+                        for j in range(len(self.childNodes[i])):
+                            self.childNodes[i][j].printTree()
+                    else:
+                        self.childNodes[i].printTree(level+1)
 
 
 
 def tabla( AST, imprime = True):
-    
     return 0
+
+
 
 def semantica(AST, imprime = True):
     return 0
