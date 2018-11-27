7ea5913a0f7e7c0ab3fdcf1b8f07df44ba050e90
Biller17 <abiller95@gmail.com>
2018-11-26 22:22:57 -0600

    yes its final version kthxbye

--- 2nd Partial/Project4/cgen.py
+++ 2nd Partial/Project4/cgen.py
@@ -4,140 +4,142 @@ from semantica import *
 codigo basado en tutoriales de MIPS https://www.youtube.com/watch?v=0aexcR9CNcE'''
 
 
-fileCode = ["file"]
+fileCode = []
 registry = {'$zero': None, '$v0': None, '$v1': None, '$a0':None, '$a1': None, '$a2': None, '$a3': None, '$t0': None, '$t1': None, '$t2':None,'$t3':None,'$t4':None,'$t5':None,'$t6':None,'$t7':None, '$s0': None, '$s1': None,'$s1': None,'$s3': None,'$s4': None,'$s5': None,'$s6': None,'$s7': None}
-def codeGen(tree, file, level =0):
+def codeGen(tree, file, level = 0):
     global fileCode
-    # print((level*3)*'__', tree.type)
+    # fileCode.append((level*3)*'__', tree.type)
     if tree.type == 'program':
-        print('.text')
-        print('.align 2')
-        print('.globl main')
+        fileCode.append('.text')
+        fileCode.append('.align 2')
+        fileCode.append('.globl main')
         codeGen(tree.childNodes[0], file, level +1)
 
     elif tree.type == "fun-declaration":
-        print(tree.childNodes[1].value, ":")
-        print('     move $fp $sp')
-        print('     sw $ra 0($sp)')
-        print('     addiu $sp $sp ‐4')
+        fileCode.append(tree.childNodes[1].value + ":")
+        fileCode.append('     move $fp $sp')
+        fileCode.append('     sw $ra 0($sp)')
+        fileCode.append('     addiu $sp $sp ‐4')
         codeGen(tree.childNodes[2][1], file, level +1)
-        print('     lw $ra 4($sp)')
-        print('     addiu $sp $sp z')
-        print('     lw $fp 0($sp)')
-        print('     jr $ra')
+        fileCode.append('     lw $ra 4($sp)')
+        fileCode.append('     addiu $sp $sp z')
+        fileCode.append('     lw $fp 0($sp)')
+        fileCode.append('     jr $ra')
 
 
     elif tree.type == "call":
+        # fileCode.append('                                                 ',tree.childNodes[0].value)
         if tree.childNodes[0].value == 'input':
             input()
         elif tree.childNodes[0].value == 'output':
             output()
         else:
-            print('     sw $fp 0($sp)')
-            print('     addiu $sp $sp ‐4')
+            fileCode.append('     sw $fp 0($sp)')
+            fileCode.append('     addiu $sp $sp ‐4')
             n = len(tree.childNodes[1].childNodes)-1
             for i in range(n):
                 codeGen(tree.childNodes[1].childNodes[n-i], file, level+1)
-                print('     sw $fp 0($sp)')
-                print('     addiu $sp $sp ‐4')
-            print("     jal ", tree.childNodes[0].value)
-            # print(len(tree.childNodes[1].childNodes))
+                fileCode.append('     sw $fp 0($sp)')
+                fileCode.append('     addiu $sp $sp ‐4')
+            fileCode.append("     jal " + tree.childNodes[0].value)
+            # fileCode.append(len(tree.childNodes[1].childNodes))
 
 
     elif tree.type == 'additive-expression' and len(tree.childNodes) == 2:
         if tree.childNodes[1].childNodes[0].type == '-':
-            # print(tree.childNodes[0].type)
+            # fileCode.append(tree.childNodes[0].type)
             codeGen(tree.childNodes[0], file, level+1)
-            print('	sw $a0 0($sp)')
-            print('	addiu $sp $sp ‐4')
+            fileCode.append('     sw $a0 0($sp)')
+            fileCode.append('     addiu $sp $sp ‐4')
             codeGen(tree.childNodes[1].childNodes[1], file, level +1)
-            print('	lw $t1 4($sp)')
-            print('	sub $a0 $t1 $a0')
-            print('	addiu $sp $sp 4')
+            fileCode.append('     lw $t1 4($sp)')
+            fileCode.append('     sub $a0 $t1 $a0')
+            fileCode.append('     addiu $sp $sp 4')
         elif tree.childNodes[1].childNodes[0].type == '+':
             codeGen(tree.childNodes[0], file, level+1)
-            print('	sw $a0 0($sp)')
-            print('	addiu $sp $sp ‐4')
+            fileCode.append('     sw $a0 0($sp)')
+            fileCode.append('     addiu $sp $sp ‐4')
             codeGen(tree.childNodes[1].childNodes[1], file, level +1)
-            print('	lw $t1 4($sp)')
-            print('	add $a0 $t1 $a0')
-            print('	addiu $sp $sp 4')
+            fileCode.append('     lw $t1 4($sp)')
+            fileCode.append('     add $a0 $t1 $a0')
+            fileCode.append('     addiu $sp $sp 4')
 
     elif tree.type == 'term' and len(tree.childNodes) > 1:
         if tree.childNodes[1].type =='term-p':
             if tree.childNodes[1].childNodes[0].type == '*':
                 codeGen(tree.childNodes[0], file, level +1)
-                print('     sw $a0 0($sp)')
-                print('     addiu $sp $sp ‐4')
+                fileCode.append('     sw $a0 0($sp)')
+                fileCode.append('     addiu $sp $sp ‐4')
                 codeGen(tree.childNodes[1].childNodes[1], file, level +1)
-                print('     lw $t1 4($sp)')
-                print('     mult $a0 $t1 $a0')
-                print('     addiu $sp $sp 4')
+                fileCode.append('     lw $t1 4($sp)')
+                fileCode.append('     mult $a0 $t1 $a0')
+                fileCode.append('     addiu $sp $sp 4')
             elif tree.childNodes[1].childNodes[0].type == '/':
                 codeGen(tree.childNodes[0], file)
-                print('      sw $a0 0($sp)')
-                print('      addiu $sp $sp ‐4')
+                fileCode.append('      sw $a0 0($sp)')
+                fileCode.append('      addiu $sp $sp ‐4')
                 codeGen(tree.childNodes[1].childNodes[1], file, level +1)
-                print('      lw $t1 4($sp)')
-                print('      div $a0 $t1 $a0')
-                print('      addiu $sp $sp 4')
+                fileCode.append('      lw $t1 4($sp)')
+                fileCode.append('      div $a0 $t1 $a0')
+                fileCode.append('      addiu $sp $sp 4')
 
     elif tree.type == 'expression':
         if tree.childNodes[1].type == '=':
             codeGen(tree.childNodes[2], file, level)
-            print('     la ', getAvailableTempVar(), '($v1)')
+            fileCode.append('     la ' + getAvailableTempVar() + '($v1)')
 
     elif tree.type == 'iteration-stmt':
-            print('     while:')
+            fileCode.append('     while:')
             codeGen(tree.childNodes[1], file, level +1)
             #todavia no se como poner el condicional
-            print('     exit')
+            fileCode.append('     exit')
             codeGen(tree.childNodes[2], file, level +1)
-            print('     j while')
-            print('     exit:')
+            fileCode.append('     j while')
+            fileCode.append('     exit:')
 
     elif(tree.type == 'selection-stmt'):
         if tree.childNodes[1].childNodes[1].childNodes[1].type  == '==':
-            print('     beq $t0, $t1, true_branch')
+            fileCode.append('     beq $t0, $t1, true_branch')
         elif tree.childNodes[1].childNodes[1].childNodes[1].type  == '<':
-            print('     slt $t3,$t1,$t0') #s0 > s1
-            print('     beq $t3, 1 true_branch')
+            fileCode.append('     slt $t3,$t1,$t0') #s0 > s1
+            fileCode.append('     beq $t3, 1 true_branch')
         elif tree.childNodes[1].childNodes[1].childNodes[1].type == '>':
-            print('     slt $t3,$t1,$t0') #s0 < s1
-            print('     beq $t3, 0 true_branch')
+            fileCode.append('     slt $t3,$t1,$t0') #s0 < s1
+            fileCode.append('     beq $t3, 0 true_branch')
         elif tree.childNodes[1].childNodes[1].childNodes[1].type  == '=<':
-            print('     beq $t0, $t1, true_branch')
-            print('     slt $t3,$t1,$t0') #s0 > s1
-            print('     beq $t3, 1 true_branch')
+            fileCode.append('     beq $t0, $t1, true_branch')
+            fileCode.append('     slt $t3,$t1,$t0') #s0 > s1
+            fileCode.append('     beq $t3, 1 true_branch')
         elif tree.childNodes[1].childNodes[1].childNodes[1].type == '=>':
-            print('     beq $t0, $t1, true_branch')
-            print('     slt $t3,$t1,$t0') #s0 < s1
-            print('     beq $t3, 0 true_branch')
+            fileCode.append('     beq $t0, $t1, true_branch')
+            fileCode.append('     slt $t3,$t1,$t0') #s0 < s1
+            fileCode.append('     beq $t3, 0 true_branch')
         elif tree.childNodes[1].childNodes[1].childNodes[1].type == '!=':
-            print('     beq $t0, $t1, false_branch')
+            fileCode.append('     beq $t0, $t1, false_branch')
 
         if tree.childNodes[3].type == 'else':
-            print('false_branch:')
+            fileCode.append('false_branch:')
             codeGen(tree.childNodes[4], file, level+1)
-            print('     b end_if')
-        print("true_branch:")
+            fileCode.append('     b end_if')
+        fileCode.append("true_branch:")
         codeGen(tree.childNodes[2], file, level +1)
-        print("end_if:")
+        fileCode.append("end_if:")
 
 
     elif(tree.type == 'return-stmt'):
         codeGen(tree.childNodes[1][0].childNodes[1], file, level +1)
-        print('     la $v0, $t3')
-        print('     lw $fp 0($sp)')
-        print('     jr $ra')
+        fileCode.append('     la $v0, $t3')
+        fileCode.append('     lw $fp 0($sp)')
+        fileCode.append('     jr $ra')
         # codeGen(tree.childNodes[0].type)
 
     elif tree.type == 'var-declaration':
-        # print(tree.childNodes[1].value)
-        print('     ori ' + getAvailableVar(tree.childNodes[1].value) + ', 0')
+        # fileCode.append(tree.childNodes[1].value)
+        fileCode.append('     ori ' + getAvailableVar(tree.childNodes[1].value) + ', 0')
 
 
     elif type(tree.childNodes) == list:
+        # fileCode.append('                                                                             ',tree.type)
         for i in range(len(tree.childNodes)):
             if(type(tree.childNodes[i]) == list):
                 for j in range(len(tree.childNodes[i])):
@@ -147,8 +149,8 @@ def codeGen(tree, file, level =0):
             else:
                 codeGen(tree.childNodes[i],file, level +1)
     if tree.type == 'endfile':
-        print('     li $v0, 10')
-        print('     syscall')
+        fileCode.append('     li $v0, 10')
+        fileCode.append('     syscall')
         generateFile(file)
 
 
@@ -156,17 +158,16 @@ def codeGen(tree, file, level =0):
 
 
 def input():
-    print('     li $v0, 5')
-    print('     syscall')
-    print('     move $t0, $v0')
-    # print('     jr $ra')
+    fileCode.append('     li $v0, 5')
+    fileCode.append('     syscall')
+    fileCode.append('     move $t0, $v0')
     return
 
 def output():
-    print('     li $v0, 1')
-    print('     move $a0, $t0')
-    print('     syscall')
-    # print('     jr $ra')
+    fileCode.append('     li $v0, 1')
+    fileCode.append('     move $a0, $t0')
+    fileCode.append('     syscall')
+    return
 
 
 
@@ -185,21 +186,25 @@ def getAvailableVar(var):
             return index
 
 def generateFile(file):
-    f= open(file,"w+")
+    print("Generador de codigo Adrian Biller A01018940")
+    open(file, 'w').close()
+    f= open(file,"a")
     for i in range(len(fileCode)):
-        f.write(fileCode[i])
+        temp = fileCode[i] + "\n"
+        f.write(temp)
 
     f.close()
 
 
 if __name__ == '__main__':
-    print("Generador de codigo Adrian Biller A01018940")
+
 
     AST = sem()
     codeGen(AST, "codeGenerated.s", 0)
     print(registry)
-    f= open("codeGenerated.s","w+")
+    f= open("codeGenerated.s","a")
     for i in range(len(fileCode)):
-        f.write(fileCode[i])
-
+        temp = fileCode[i] + "\n"
+        print(temp)
+        f.write(temp)
     f.close()
--- 2nd Partial/Project4/codeGenerated.s
+++ 2nd Partial/Project4/codeGenerated.s
@@ -1,1 +1,41 @@
-file
+.text
+.align 2
+.globl main
+gcd:
+     move $fp $sp
+     sw $ra 0($sp)
+     addiu $sp $sp ‐4
+     beq $t0, $t1, true_branch
+false_branch:
+     sw $fp 0($sp)
+     addiu $sp $sp ‐4
+     sw $fp 0($sp)
+     addiu $sp $sp ‐4
+     jal gcd
+     la $v0, $t3
+     lw $fp 0($sp)
+     jr $ra
+     b end_if
+true_branch:
+     la $v0, $t3
+     lw $fp 0($sp)
+     jr $ra
+end_if:
+     lw $ra 4($sp)
+     addiu $sp $sp z
+     lw $fp 0($sp)
+     jr $ra
+main:
+     move $fp $sp
+     sw $ra 0($sp)
+     addiu $sp $sp ‐4
+     ori $s0, 0
+     ori $s1, 0
+     la $t7($v1)
+     la $t6($v1)
+     lw $ra 4($sp)
+     addiu $sp $sp z
+     lw $fp 0($sp)
+     jr $ra
+     li $v0, 10
+     syscall
